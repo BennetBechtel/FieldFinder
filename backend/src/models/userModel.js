b8cd4af33af936bfc37d886f1e116d67713440f1
Bennet Bechtel <bennet.bechtel007@gmail.com>
2024-03-23 20:57:55 +0100

    user model

---
+++ backend/src/models/userModel.js
@@ -0,0 +1,25 @@
+import mongoose, { Schema } from "mongoose";
+
+const UserSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const UserModel = mongoose.model("User", UserSchema);
+
+export default UserModel;
