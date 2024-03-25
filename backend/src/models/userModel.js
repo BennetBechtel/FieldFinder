b4c2e6f7c4045d035b8b7f194f2842b174b27290
Bennet Bechtel <bennet.bechtel007@gmail.com>
2024-03-25 16:48:07 +0100

    user model

---
+++ backend/src/models/userModel.js
@@ -0,0 +1,21 @@
+import { Schema } from "mongoose";
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+})
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,10 +4,12 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+/*   
   server: {
     port: 5173,
     proxy: {
       "/api": "http://localhost:5050",
     },
-  },
+  }, 
+*/
 });
